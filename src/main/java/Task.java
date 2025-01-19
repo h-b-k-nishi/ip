31da0b761f562fff3a07bac8f6b1b6c20729675b
h-b-k-nishi <>
2025-01-19 20:28:30 +0800

    Change the name of field from name to description

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -4,23 +4,23 @@
  * @author Hibiki Nishiwaki
  */
 public class Task {
-    private final String name;
+    private final String description;
 
     /**
      * The constructor of task.
      * 
-     * @param name The name of task.
+     * @param description The description of task.
      */
-    public Task(String name) {
-        this.name = name;
+    public Task(String description) {
+        this.description = description;
     }
 
     /**
      * Return the string representation of this task.
      * 
-     * @return The string consisting of the name of task.
+     * @return The string consisting of the description of task.
      */
     public String toString() {
-        return this.name;
+        return this.description;
     }
 }
