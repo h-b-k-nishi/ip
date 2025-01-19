bfb48d9d46771f5de7f3629964ed56bde7d78e2a
h-b-k-nishi <>
2025-01-19 19:05:46 +0800

    Rename chatbot to Pochi

---
+++ src/main/java/Pochi.java
@@ -0,0 +1,9 @@
+public class Pochi {
+    public static void main(String[] args) {
+        String greet = "Hello! I'm Pochi.\n"
+                + "What can I do for you?\n";
+        String farewell = "Bye. Hope to see you again soon!\n";
+        System.out.println(greet);
+        System.out.println(farewell);
+    }
+}
