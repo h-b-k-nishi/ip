cb1b83555527edd01d6ddd8d6fc2a91daaef8a15
h-b-k-nishi <>
2025-01-19 19:43:16 +0800

    Implement echo feature

--- src/main/java/Pochi.java
+++ src/main/java/Pochi.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * A class represents a chatbot Pochi.
  * 
@@ -6,9 +8,16 @@
 public class Pochi {
     public static void main(String[] args) {
         String greet = "Hello! I'm Pochi.\n"
-                + "What can I do for you?\n";
-        String farewell = "Bye. Hope to see you again soon!\n";
+                + "What can I do for you?\n",
+                farewell = "Bye. Hope to see you again soon!\n";
         System.out.println(greet);
+        
+        Scanner sc = new Scanner(System.in);
+        while (true){
+            String command = sc.nextLine();
+            if (command.equals("bye")) break;
+            System.out.println(command);
+        }
         System.out.println(farewell);
     }
 }
